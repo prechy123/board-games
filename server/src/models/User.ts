cd9b880cd5f3f96ac14fc0ca5ad80ac8e0603ec0
temitope david dairo <dairotemitopedavid@gmail.com>
2024-10-05 09:07:58 -0700

    update message

---
+++ server/src/models/User.ts
@@ -0,0 +1,32 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+export interface IUser extends Document {
+    email: string;
+    password: string;
+    resetPasswordToken?: string;
+    resetPasswordExpires?: Date;
+    comparePassword(password: string): Promise<boolean>;
+}
+
+const UserSchema: Schema<IUser> = new Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
+});
+
+// Hash the password before saving
+UserSchema.pre<IUser>('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+// Add a method to compare passwords
+UserSchema.methods.comparePassword = function (password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.password);
+};
+
+export default mongoose.model<IUser>('User', UserSchema);
