089680901b738f75aedaec6d6379a43b70d569c9
prechy123 <bamideleprecious100@gmail.com>
2024-10-12 00:23:09 +0100

    update

--- server/src/server.ts
+++ server/src/server.ts
@@ -26,6 +26,8 @@ const corsOption = {
 };
 
 const app = express();
+app.set('trust proxy', true);
+
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
