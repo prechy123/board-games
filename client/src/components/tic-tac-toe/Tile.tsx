680d461cd779d0cf408f30157cbe3082a6a0bbdf
prechy123 <bamideleprecious100@gmail.com>
2024-10-11 18:54:50 +0100

    update

--- client/src/components/tic-tac-toe/Tile.tsx
+++ client/src/components/tic-tac-toe/Tile.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import "./tile.css";
+import * as motion from "framer-motion/client";
+
+const draw = {
+  hidden: { pathLength: 0, opacity: 0 },
+  visible: (i) => {
+    const delay = i * 0.5;
+    return {
+      pathLength: 1,
+      opacity: 1,
+      transition: {
+        pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
+        opacity: { delay, duration: 0.01 },
+      },
+    };
+  },
+};
+
 export default function Tile({
   value,
   handleClick,
@@ -7,10 +27,52 @@ export default function Tile({
 }) {
   return (
     <span
-      className=" w-16 h-16 bg-white text-red-500 text-3xl flex items-center justify-center"
+      className=" w-16 sm:w-28 h-16 sm:h-28 bg-white text-red-500 text-3xl flex items-center justify-center"
       onClick={handleClick}
     >
-      {value}
+      {value === "-" ? (
+        value
+      ) : (
+        <motion.svg
+          width="300"
+          height="300"
+          viewBox="0 0 300 300"
+          initial="hidden"
+          animate="visible"
+        >
+          {value === "O" ? (
+            <motion.circle
+              cx="150"
+              cy="150"
+              r="80"
+              stroke="#ff0055"
+              variants={draw}
+              custom={0}
+            />
+          ) : (
+            <>
+              <motion.line
+                x1="75"
+                y1="75"
+                x2="225"
+                y2="225"
+                stroke="#00cc88"
+                variants={draw}
+                custom={0}
+              />
+              <motion.line
+                x1="75"
+                y1="225"
+                x2="225"
+                y2="75"
+                stroke="#00cc88"
+                variants={draw}
+                custom={0.5}
+              />
+            </>
+          )}
+        </motion.svg>
+      )}
     </span>
   );
 }
